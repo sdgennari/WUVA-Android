56eb07a88ff088c07513518559bccb83665b91e0
spencer.gennari <sdgennari@gmail.com>
2016-04-20 02:35:59 -0400

    Fixed bug in StringUtils to handle f/ and w/

--- android-application/app/src/main/java/com/poofstudios/android/wuvaradio/utils/StringUtils.java
+++ android-application/app/src/main/java/com/poofstudios/android/wuvaradio/utils/StringUtils.java
@@ -9,14 +9,22 @@ public class StringUtils {
         if (string.isEmpty()) {
             return string;
         }
+
         string = string.toLowerCase();
+
+        // Handle f/<name> and w/<name> explicitly
+        string = string.replaceAll("f/", "f/ ").replaceAll("w/", "w/ ");
+
         String[] words = string.split(" ");
         String result = "";
         for(String word : words) {
             if (word.isEmpty()) {
                 continue;
+            } else if (word.equals("f/") || word.equals("w/")) {
+                result += String.format("%s ", word);
+            } else {
+                result += String.format("%s%s ", word.substring(0, 1).toUpperCase(), word.substring(1));
             }
-            result += word.substring(0, 1).toUpperCase() + word.substring(1) + " ";
         }
         return result.trim();
     }
